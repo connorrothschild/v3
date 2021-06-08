d85f9506accf54888522af54f7a209a33e3afaee
Connor Rothschild <connorrothschild@gmail.com>
2021-06-08 14:46:13 -0500

    lil reading time note

--- components/ReadingTime.vue
+++ components/ReadingTime.vue
@@ -1,11 +1,18 @@
 <template>
-  <span>{{ readingTime }} minute read</span>
+  <span @mouseover="hovered = true" @mouseout="hovered = false">{{ readingTime }} minute read 
+    <span class='wpm' v-if="hovered">(at 200 WPM)</span>
+    </span>
 </template>
 
 <script>
 export default {
   name: "readingTime",
   props: ["content"],
+  data: function () {
+      return {
+        hovered: false
+      }
+  },
   computed: {
     readingTime() {
       let minutes = 0;
@@ -18,3 +25,11 @@ export default {
   },
 };
 </script>
+
+<style>
+.wpm {
+  text-transform: none;
+  font-size: .8rem;
+  font-weight: 200;
+}
+</style>
