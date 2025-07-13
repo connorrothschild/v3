e36adc2f1e421ab43ddb5850e956de0fdc86ee95
Connor Rothschild <connorrothschild@gmail.com>
2025-07-13 15:01:52 -0500

    Add robots.txt to disallow indexing

---
+++ dist/_nuxt/static/1752436817/award/pudding-cup-2020/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1752436817",layout:"default",error:a,serverRendered:true,routePath:"\u002Faward\u002Fpudding-cup-2020",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ce61e8e8"}}}}(null));
---
+++ dist/_nuxt/static/1752436817/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1752436817",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ce61e8e8"}}}}(null,"\u002F"));
