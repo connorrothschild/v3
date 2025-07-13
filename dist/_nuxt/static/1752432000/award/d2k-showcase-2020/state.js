3cf2d87f7e27e29cf59b52a7a83bc3b4cbccb504
Connor Rothschild <connorrothschild@gmail.com>
2025-07-13 13:45:46 -0500

    Add properly built dist folder

---
+++ dist/_nuxt/static/1752432000/award/d2k-showcase-2020/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1752432000",layout:"default",error:a,serverRendered:true,routePath:"\u002Faward\u002Fd2k-showcase-2020",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dce3aad3"}}}}(null));
